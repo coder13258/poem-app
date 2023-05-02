b11c9ade18fa3d64bb2893d53c63608fdf791af9
coder13258 <132388162+coder13258@users.noreply.github.com>
2023-05-02 18:27:57 +0100

    Update App.js with generated code

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import 'tailwindcss/tailwind.css';
 
-function App() {
+const App = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [category, setCategory] = useState('');
+  const [poems, setPoems] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newPoem = { title, content, category, id: Date.now() };
+    setPoems([...poems, newPoem]);
+    setTitle('');
+    setContent('');
+    setCategory('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold mb-8">Poem App</h1>
+      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full max-w-md">
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Title"
+          required
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4"
+        />
+        <textarea
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          placeholder="Poem content"
+          required
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4 h-32 resize-none"
+        />
+        <input
+          type="text"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          placeholder="Category"
+          required
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4"
+        />
+        <button type="submit" className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+          Save Poem
+        </button>
+      </form>
+      <div className="w-full max-w-md">
+        {poems.map((poem) => (
+          <div key={poem.id} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <h2 className="text-xl font-bold mb-2">{poem.title}</h2>
+            <p className="text-gray-600 mb-4">{poem.category}</p>
+            <pre className="whitespace-pre-wrap">{poem.content}</pre>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
